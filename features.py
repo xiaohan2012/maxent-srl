83488b00257c190667257ffa5b93099682b688cf
Han Xiao <han.xiao@cs.helsinki.fi>
2015-03-20 21:45:06 +0200

    added positoin, path2frame and phrasetype features

--- features.py
+++ features.py
@@ -1,5 +1,7 @@
 from nltk.stem import PorterStemmer
+
 from ling_util import get_head_word
+from data import (Context, Frame, NodePosition)
 
 class Feature(object):
     @classmethod
@@ -7,11 +9,112 @@ class Feature(object):
         raise NotImplementedError
 
 class Position(Feature):
+    """
+    >>> from nltk.tree import Tree
+    >>> from data import (Context, Frame, NodePosition)
+    >>> tree = Tree('ROOT', [Tree('S', [Tree('NP', [Tree('NP', [Tree('PRP$', ['Your']), Tree('NN', ['contribution'])]), Tree('PP', [Tree('TO', ['to']), Tree('NP', [Tree('NNP', ['Goodwill'])])])]), Tree('VP', [Tree('MD', ['will']), Tree('VP', [Tree('VB', ['mean']), Tree('ADVP', [Tree('ADVP', [Tree('RBR', ['more'])]), Tree('SBAR', [Tree('IN', ['than']), Tree('S', [Tree('NP', [Tree('PRP', ['you'])]), Tree('VP', [Tree('MD', ['may']), Tree('VP', [Tree('VB', ['know'])])])])])])])]), Tree('.', ['.'])])])
+    >>> sent = u'Your contribution to Goodwill will mean more than you may know .'
+    >>> Position.get_value(tree[0][0][0][0], Context(sent, tree, Frame(start=5, end=16, name='Giving'), NodePosition(0, 3)))
+    'strict-before'
+    >>> Position.get_value(tree[0][1][0], Context(sent, tree, Frame(start=5, end=16, name='Giving'), NodePosition(18, 19)))
+    'strict-after'
+    >>> Position.get_value(tree[0][0][0][1], Context(sent, tree, Frame(start=5, end=16, name='Giving'), NodePosition(5, 16)))
+    'in'
+    >>> Position.get_value(tree[0][0], Context(sent, tree, Frame(start=21, end=33, name='FakeStuff'), NodePosition(0, 28)))
+    'overlap-before'
+    >>> Position.get_value(tree[0][1], Context(sent, tree, Frame(start=21, end=33, name='FakeStuff'), NodePosition(28, 37)))
+    'overlap-after'
+    """
+    name = "pos_to_frame"
+    
     @classmethod
     def get_value(cls, u, c):
-        pass
+        if c.node_pos.end < c.frame.start:
+            return 'strict-before'
+        elif c.node_pos.start > c.frame.end:
+            return 'strict-after'
+        elif c.node_pos.start >= c.frame.start and c.node_pos.end <= c.frame.end:
+            return 'in'
+        elif c.node_pos.end > c.frame.start and c.node_pos.start < c.frame.start:
+            return 'overlap-before'
+        elif c.node_pos.start < c.frame.end and c.node_pos.end > c.frame.end:
+            return 'overlap-after'
+        else:
+            raise ValueError('Invalid position in case of  %r and %r' %(c.node_pos, c.frame))
+
+class PathToFrame(Feature):
+    """
+    >>> from nltk.tree import Tree
+    >>> from data import (Context, Frame, NodePosition)
+    >>> tree = Tree('ROOT', [Tree('S', [Tree('NP', [Tree('NP', [Tree('PRP$', ['Your']), Tree('NN', ['contribution'])]), Tree('PP', [Tree('TO', ['to']), Tree('NP', [Tree('NNP', ['Goodwill'])])])]), Tree('VP', [Tree('MD', ['will']), Tree('VP', [Tree('VB', ['mean']), Tree('ADVP', [Tree('ADVP', [Tree('RBR', ['more'])]), Tree('SBAR', [Tree('IN', ['than']), Tree('S', [Tree('NP', [Tree('PRP', ['you'])]), Tree('VP', [Tree('MD', ['may']), Tree('VP', [Tree('VB', ['know'])])])])])])])]), Tree('.', ['.'])])])
+    >>> sent = u'Your contribution to Goodwill will mean more than you may know .'
+    >>> PathToFrame.get_value(tree[0][0][0][0], Context(sent, tree, Frame(start=5, end=16, name='Giving'), NodePosition(0, 3))) # your
+    ('PRP$', 'u', 'NP', 'd', 'NN')
+    >>> PathToFrame.get_value(tree[0][1][0], Context(sent, tree, Frame(start=5, end=16, name='Giving'), NodePosition(18, 19))) # to
+    ('TO', 'u', 'PP', 'u', 'NP', 'd', 'NP', 'd', 'NN')
+    >>> PathToFrame.get_value(tree[0][0][0][1], Context(sent, tree, Frame(start=5, end=16, name='Giving'), NodePosition(35, 38))) #mean
+    ('VB', 'u', 'VP', 'u', 'VP', 'u', 'S', 'd', 'NP', 'd', 'NP', 'd', 'NN')
+    """
+    name = 'path_to_frame'
+
+    @classmethod
+    def get_word_index_range(cls, sent, char_start, char_end):
+        """Given the char start/end index, return the corresponding word start/end index"""
+        cur = 0
+        start, end = None, None
+        for i, w in enumerate(sent.split()):
+            if cur == char_start:
+                start = i
+            if (cur+len(w)-1) == char_end:
+                end = i+1
+                break
+            cur += (len(w)+1)
+
+        assert start is not None
+        assert end is not None
+        return start, end
+    
+    @classmethod
+    def get_value(cls, u, c):
+        #from root to source node
+        start, end = cls.get_word_index_range(c.sentence, c.node_pos.start, c.node_pos.end)
+        path1 = c.parse_tree.treeposition_spanning_leaves(start, end)
+
+        #from root to frame node
+        start, end = cls.get_word_index_range(c.sentence, c.frame.start, c.frame.end)
+        path2 = c.parse_tree.treeposition_spanning_leaves(start, end)
+        
+        #find their lowest common ancestor
+        i = 0
+        lca = c.parse_tree
+        while path1[i] == path2[i]:
+            lca = lca[path1[i]]
+            i += 1
+
+        steps = [lca.label()]
+        # source node to LCA
+        node = lca
+        for ind in path1[i:-1]: #ignore the leaf node, so -1
+            steps.insert(0, 'u')
+            node = node[ind]
+            steps.insert(0, node.label())
+
+        # LCA to frame node
+        node = lca
+        for ind in path2[i:-1]: #ignore the leaf node, so -1
+            steps.append('d')
+            node = node[ind]
+            steps.append(node.label())
+
+        return tuple(steps)
 
 class PhraseType(Feature):
+    """
+    >>> from nltk.tree import Tree
+    >>> tree = Tree('ROOT', [Tree('S', [Tree('NP', [Tree('NP', [Tree('PRP$', ['Your']), Tree('NN', ['contribution'])]), Tree('PP', [Tree('TO', ['to']), Tree('NP', [Tree('NNP', ['Goodwill'])])])]), Tree('VP', [Tree('MD', ['will']), Tree('VP', [Tree('VB', ['mean']), Tree('ADVP', [Tree('ADVP', [Tree('RBR', ['more'])]), Tree('SBAR', [Tree('IN', ['than']), Tree('S', [Tree('NP', [Tree('PRP', ['you'])]), Tree('VP', [Tree('MD', ['may']), Tree('VP', [Tree('VB', ['know'])])])])])])])]), Tree('.', ['.'])])])
+    >>> PhraseType.get_value(tree[0][0], None)
+    'NP'
+    """
     name = "phrase_type"
     
     @classmethod
